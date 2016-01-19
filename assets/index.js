892dbb15facc7694fdf99c4374401c88f79eb621
Tyler Long <tyler4long@gmail.com>
2016-01-19 15:28:39 +0800

    shiftWindow

--- assets/index.js
+++ assets/index.js
@@ -7,15 +7,14 @@ mermaid.ganttConfig = {
 };
 
 
+// fix navbar overlap anchor links issue.
 function shiftWindow() {
   scrollBy(0, -64);
 }
 
 
 $(function() {
-  if (location.hash) {
-    shiftWindow();
-  }
+  setTimeout(function() { shiftWindow(); }, 128);
   window.addEventListener("hashchange", shiftWindow);
   mermaid.init(undefined, $('article.markdown-body .mermaid'));
 });
