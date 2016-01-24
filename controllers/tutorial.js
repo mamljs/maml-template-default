70017b46abb5f51ba28b2b862dcea48a0beb0fbf
Tyler Long <tyler4long@gmail.com>
2016-01-24 17:06:58 +0800

    Add tutorial page

---
+++ controllers/tutorial.js
@@ -0,0 +1,23 @@
+var mdc = require('markdown-core/markdown-core-node');
+var cheerio = require('cheerio');
+
+
+function index(page) {
+  mdc.map = true;
+  var html = mdc.render(page.markdown);
+  mdc.map = false;
+  var $ = cheerio.load(html);
+  var sidebarMD = '';
+  $('h1').each(function(idx, h1) {
+    sidebarMD += `${idx}. <a href="#${$(h1).attr('id')}">${$(h1).text()}</a>\n`;
+  });
+  page.sidebar = mdc.render(sidebarMD);
+
+  page.html = mdc.render(page.markdown);
+  page.generate();
+}
+
+
+module.exports = {
+  index: index,
+};
