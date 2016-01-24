4d03249cfd185eb560295d821067f8bbaea7ff04
Tyler Long <tyler4long@gmail.com>
2016-01-24 21:37:39 +0800

    First working version of docs page

--- controllers/docs.js
+++ controllers/docs.js
@@ -2,19 +2,80 @@ var mdc = require('markdown-core/markdown-core-node');
 var cheerio = require('cheerio');
 
 
-function index(page) {
-  mdc.map = true;
-  var html = mdc.render(page.markdown);
-  mdc.map = false;
-  var $ = cheerio.load(html);
-  var sidebarMD = '';
-  $('h1').each(function(idx, h1) {
-    sidebarMD += `${idx}. <a href="#${$(h1).attr('id')}">${$(h1).text()}</a>\n`;
-  });
-  page.sidebar = mdc.render(sidebarMD);
+function getHTML(markdown) {
+  return mdc.render(markdown);
+}
 
-  page.html = mdc.render(page.markdown);
-  page.generate();
+
+// get html with data-source-line attributes
+function getMappedHTML(markdown) {
+  mdc.map = true;
+  var html = mdc.render(markdown);
+  mdc.map = false;
+  return html;
+}
+
+
+// split markdown by <h1>
+function split(markdown) {
+  var html = getMappedHTML(markdown);
+  var lines = markdown.split('\n');
+  var $ = cheerio.load(html);
+  var pages = $('h1').toArray().map(h1 => {
+    return {
+      id: $(h1).attr('id'),
+      name: $(h1).text(),
+      source_line: $(h1).data('source-line') - 1,
+    };
+  });
+  for(var i = 0; i < pages.length - 1; i++) {
+    var page = pages[i];
+    var next_page = pages[i + 1];
+    page.markdown = lines.slice(page.source_line + 1, next_page.source_line).join('\n');
+    page.html = `<h1>${page.name}</h1>\n` + getHTML(page.markdown);
+  }
+  page = pages[pages.length - 1];
+  page.markdown = lines.slice(page.source_line + 1, -1).join('\n');
+  page.html = `<h1>${page.name}</h1>\n` + getHTML(page.markdown);
+  return pages;
+}
+
+
+// get html code for sidebar
+function sidebar(base_pathname, pages, idx) {
+  var sidebarMD = '';
+  for(var i = 0; i < pages.length; i++) {
+    var page = pages[i];
+    var pathname = base_pathname; // i == 0
+    if(i > 0) {
+      pathname += `${pages[i].id}/`;
+    }
+    if(i == idx) { // current link
+      sidebarMD += `${i}. ${page.name}\n`;
+    } else {
+      sidebarMD += `${i}. <a href="${pathname}">${page.name}</a>\n`;
+    }
+  }
+  return mdc.render(sidebarMD);
+}
+
+
+// the action method
+function index(page) {
+  var pages = split(page.markdown);
+  var base_pathname = page.pathname;
+  for(var i = 0; i < pages.length; i++) {
+    var pathname = base_pathname; // i == 0
+    if(i > 0) {
+      pathname += `${pages[i].id}/`;
+    }
+    page.pathname = pathname;
+    page.sidebar = sidebar(base_pathname, pages, i);
+    page.markdown = pages[i].markdown;
+    page.html = pages[i].html;
+    page.title = pages[i].name;
+    page.generate();
+  }
 }
 
 
